29e21b59918d9d28fc1250fd513c6fb627ff5c3b
Joao Machado <joao.machado@fanduel.com>
2024-06-29 23:54:37 +0100

    improve contributors text

--- photo-gallery/src/components/Contributors.js
+++ photo-gallery/src/components/Contributors.js
@@ -18,7 +18,7 @@ const Contributors = () => {
             My name is Luís Correia Gomes, and at 30 years old, I am in the
             final year of my residency at Instituto Português de Oncologia de
             Lisboa Francisco Gentil in Portugal and a member of the Young
-            Portuguese National Endoscopic Committee (SPEDY). During the early
+            Portuguese National Endoscopic Committee (SPED). During the early
             stages of my endoscopic training, I encountered difficulties in
             finding a comprehensive endoscopic atlas. This challenge inspired me
             to start collecting endoscopic images from the beginning of my
@@ -39,21 +39,29 @@ const Contributors = () => {
             scientific, non-profit, and public utility association that brings
             together doctors and other health professionals who practice or are
             interested in digestive endoscopy in Portugal. Founded in 1979, SPED
-            aims to: Promote the development of digestive endoscopy in service
-            to the health of the Portuguese population; Disseminate updated
+            aims to: 
+            </p>
+            <ul>
+            <li>Promote the development of digestive endoscopy in service
+            to the health of the Portuguese population;</li>
+            <li> Disseminate updated
             ideas, knowledge, and research in the field of digestive endoscopy;
-            Stimulate research in digestive endoscopy; Promote national and
+            Stimulate research in digestive endoscopy;</li>
+            <li>Pathologistromote national and
             international contacts and exchanges among various professionals
-            involved in digestive endoscopy; Develop educational activities
+            involved in digestive endoscopy;</li>
+            <li> Develop educational activities
             leading to the training and improvement of doctors and other
-            technicians in digestive endoscopy; Contribute to the establishment
-            of training and practice standards in digestive endoscopy. SPED is
-            affiliated with the European Society of Gastrointestinal Endoscopy
+            technicians in digestive endoscopy;</li>
+            <li>Contribute to the establishment
+            of training and practice standards in digestive endoscopy.</li>
+            </ul>
+
+            SPED is affiliated with the European Society of Gastrointestinal Endoscopy
             and the World Endoscopy Organization. This atlas was created during
             the mandate of Prof. Susana Lopes in the biennium 2023-2025,
             reflecting SPED's commitment to fostering educational resources and
             enhancing the practice of digestive endoscopy.
-          </p>
         </div>
       </div>
 
